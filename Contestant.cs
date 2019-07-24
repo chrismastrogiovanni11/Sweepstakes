2d04911e3e23c31899f6983e687986f4dab12ed4
chrismastrogiovanni11 <chrismaastro@gmail.com>
2019-07-24 15:11:56 -0500

    made contestant class and icontestant interface, working on sweepstakes class now

---
+++ Contestant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesProject
+{
+
+    //use interface
+    class Contestant:IContestant 
+    {
+        public void FirstName()
+        {
+
+        }
+        public void LastName()
+        {
+
+        }
+
+        public void RegistrationNumber()
+        {
+
+        }
+
+        public void Email()
+        {
+
+        }
+    }
+}
---
+++ IContestant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesProject
+{
+    interface IContestant
+    {
+        void FirstName();
+        void LastName();
+        void RegistrationNumber();
+
+        void Email();
+    }
+}
