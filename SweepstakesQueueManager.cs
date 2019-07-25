420b10b7353f9f279254aebeeea0f7a4bb77fc8b
chrismastrogiovanni11 <chrismaastro@gmail.com>
2019-07-25 11:19:10 -0500

    added rest of classes. figuring out how to input registration numbers and names from dictionary into contestant class

---
+++ SweepstakesQueueManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesProject
+{
+    //use Queue as underlying structure
+    class SweepstakesQueueManager:ISweepstakesManager
+    {
+        public void InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+
+        }
+
+        public Sweepstakes GetSweepstakes()
+        {
+            Sweepstakes sweepstakes = new Sweepstakes();
+            return;
+        }
+
+
+
+    }
+}
---
+++ SweepstakesStackManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesProject
+{
+
+    //use stack as underlying structure
+    class SweepstakesStackManager : ISweepstakesManager
+    {
+        public void InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+
+        }
+
+        Sweepstakes GetSweepstakes()
+        {
+            Sweepstakes sweepstakes = new Sweepstakes();
+            
+        }
+
+    }
+    
+    
+}
